01c0d4b4f9a92f9bec4afb98a032526ca565baf8
Ana <arperezch13@alumnos.cei.es>
2026-02-05 21:44:42 +0100

    add user generation script and enhance HTML structure with personal details and skills

---
+++ Practica0.js
@@ -0,0 +1,14 @@
+const userGenerator = async () => { 
+    try {
+        const response = await fetch('https://randomuser.me/api/');
+        const data = await response.json();
+        const user = data.results[0];
+        console.log(`Nombre: ${user.name.first} ${user.name.last}`);
+        console.log(`Email: ${user.email}`);
+        console.log(`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQGDhrMTg-2Z1oRUdn5DnGkscpmk6aFi7mXg&s`)
+    } catch (error) {
+        console.error('Error fetching user data:', error);
+    }
+}
+
+userGenerator();
