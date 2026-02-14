dc6133ae41574608b21abfecc4fa7f1ff7fc7bd4
kamawebdev13 <karinadatanalyst13@alumnos.cei.es>
2026-02-14 23:22:14 +0100

    cambios

--- karina/egle/tareas-js/practicaJsUno.js
+++ karina/egle/tareas-js/practicaJsUno.js
@@ -1,7 +1,7 @@
-const products= [
-  { name: "Laptop", price: 850, stock: 15 },
-  { name: "Mouse", price: 25, stock: 50 },
-  { name: "Teclado", price: 45, stock: 20 }
+const products = [
+    { name: "Laptop", price: 850, stock: 15 },
+    { name: "Mouse", price: 25, stock: 50 },
+    { name: "Teclado", price: 45, stock: 20 }
 ];
 
 
@@ -9,7 +9,7 @@ const addCart = (productName, quantity) => {
 
     const product = products.find(p => p.name.toLowerCase() === productName.toLowerCase()
     );
-    if(!product) {
+    if (!product) {
         return console.log(`${productName} no encontrado intente de nuevo`)
     }
 
@@ -18,15 +18,15 @@ const addCart = (productName, quantity) => {
     }
 
     let total = product.price * quantity;
-   
+
     console.log(`El total de su compra es la sig: ${quantity}  del producto: ${productName}`)
 
-    let productleft = product.stock - quantity;
-    
-    console.log(`Del inventario del producto: ${productName} queda la cantidad de ${productleft}`);
+    product.stock -= quantity;
 
-    if(productleft === 0) {
-        return console.log(`Alerta el siguiente producto: ${productName} se ha agotado`)
+    console.log(`Del inventario del producto: ${product.name} queda la cantidad de ${product.stock}`);
+
+    if (product.stock === 0) {
+        return console.log(`Alerta el siguiente producto: ${product.name} se ha agotado`)
     }
 
 
@@ -34,3 +34,5 @@ const addCart = (productName, quantity) => {
 }
 
 addCart("Mouse", 50)
+
+console.log(products);
